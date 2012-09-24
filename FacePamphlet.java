02c44bb5cbe56f013aeb9351303083b2f263a39f
m2m tech <tech@m2m.at>
2012-09-24 12:03:35 +0200

    assignment #7 task #1

--- FacePamphlet.java
+++ FacePamphlet.java
@@ -11,17 +11,63 @@ import acm.util.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class FacePamphlet extends Program 
+public class FacePamphlet extends ConsoleProgram 
 					implements FacePamphletConstants {
 
+	/**
+	 * private instance variables
+	 */
+	private JTextField nameField;
+	private JButton addButton;
+	private JButton deleteButton;
+	private JButton lookupButton;
+
+	private JTextField statusField;
+	private JButton statusButton;
+	private JTextField pictureField;
+	private JButton pictureButton;
+	private JTextField friendField;
+	private JButton friendButton;
+
+	
 	/**
 	 * This method has the responsibility for initializing the 
 	 * interactors in the application, and taking care of any other 
 	 * initialization that needs to be performed.
 	 */
 	public void init() {
-		// You fill this in
-    }
+		add(new JLabel("Name:"), NORTH);
+		nameField = new JTextField(TEXT_FIELD_SIZE);
+		add(nameField, NORTH);
+		addButton = new JButton("Add");
+		add(addButton, NORTH);
+		deleteButton = new JButton("Delete");
+		add(deleteButton, NORTH);
+		lookupButton = new JButton("Lookup");
+		add(lookupButton, NORTH);
+
+		statusField = new JTextField(TEXT_FIELD_SIZE);
+		statusField.addActionListener(this);
+		add(statusField, WEST);
+		statusButton = new JButton("Change Status");
+		add(statusButton, WEST);
+		add(new JLabel(EMPTY_LABEL_TEXT), WEST);
+
+		pictureField = new JTextField(TEXT_FIELD_SIZE);
+		pictureField.addActionListener(this);
+		add(pictureField, WEST);
+		pictureButton = new JButton("Change Picture");
+		add(pictureButton, WEST);
+		add(new JLabel(EMPTY_LABEL_TEXT), WEST);
+
+		friendField = new JTextField(TEXT_FIELD_SIZE);
+		friendField.addActionListener(this);
+		add(friendField, WEST);
+		friendButton = new JButton("Add Friend");
+		add(friendButton, WEST);
+		
+		addActionListeners();
+	}
     
   
     /**
@@ -30,7 +76,36 @@ public class FacePamphlet extends Program
      * to respond to these actions.
      */
     public void actionPerformed(ActionEvent e) {
-		// You fill this in as well as add any additional methods
+    	Object source = e.getSource();
+    	if (source == addButton) {
+    		if (!emptyTextField(nameField)) {
+    			println("Add: " + nameField.getText());
+    		}    		
+    	} else if (source == deleteButton) {
+    		if (!emptyTextField(nameField)) {
+    			println("Delete: " + nameField.getText());    			
+    		}    		    		
+    	} else if (source == lookupButton) {
+    		if (!emptyTextField(nameField)) {
+    			println("Lookup: " + nameField.getText());
+    		}    		    		
+    	} else if ((source == statusField) || (source == statusButton)) {
+    		if (!emptyTextField(statusField)) {
+    			println("Change Status: " + statusField.getText());
+    		}    		
+    	} else if ((source == pictureField) || (source == pictureButton)) {
+    		if (!emptyTextField(pictureField)) {
+    			println("Change Picture: " + pictureField.getText());    			
+    		}    		
+    	} else if ((source == friendField) || (source == friendButton)) {
+    		if (!emptyTextField(friendField)) {
+    			println("Add Friend: " + friendField.getText());
+    		}    		    		
+    	}
 	}
+    
+    private boolean emptyTextField(JTextField field) {
+    	return field.getText().equals("");
+    }
 
 }
